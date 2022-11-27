59d0ed1aea3eb71305067e6be7ab4f6356d25d40
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-27 19:29:36 +0000

    buttons successfully matched to select value

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const select = document.querySelector("select");
 const options = document.querySelectorAll("option");
 const buttons = document.querySelectorAll("button");
+const optionsData = document.querySelectorAll("data-option");
+const buttonsData = document.querySelectorAll("data-button");
 
-//append suffix to each option
 function appendSuffix() {
 	options.forEach((option) => {
 		let suffix = "";
 		suffix = "th";
-		if (option.value == 1 || option.value == 21) suffix = "st";
-		if (option.value == 2 || option.value == 22) suffix = "nd";
-		if (option.value == 3 || option.value == 23) suffix = "rd";
+		if (option.textContent == 1 || option.textContent == 21) suffix = "st";
+		if (option.textContent == 2 || option.textContent == 22) suffix = "nd";
+		if (option.textContent == 3 || option.textContent == 23) suffix = "rd";
 
-		option.textContent = option.value + suffix + " of December";
+		option.textContent = option.textContent + suffix + " of December";
+	});
+}
+
+function matchButtons() {
+	buttons.forEach((button) => {
+		button.disabled = true;
+		if (button.dataset.button == select.dataset.select) button.disabled = false;
 	});
 }
 
 appendSuffix();
+
+select.addEventListener("change", () => {
+	select.dataset.select = select.value[0];
+	matchButtons();
+});
